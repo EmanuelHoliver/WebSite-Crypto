6f373487e6d05b0a95a5bfd94d88e27702bb030d
Emanuel <emanuel_heitor@hotmail.com>
2022-10-27 12:07:37 -0300

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import styled  from "styled-components";
+import image from '../assets/home.png';
+import background from '../assets/background.png';
+import Button from "./Button";
+import {RiGroupLine} from 'react-icons/ri';
+
+export default function Home() {
+    return (
+       <Section id="home">
+        <div className="text">
+            <h1>
+                <span className="highlight">Earthium</span> is a real-time statergic game
+            </h1>
+            <p> Where you earn money, 
+                <span className="highlights"> crypto</span> 
+                and <span className="highlight">NFT's </span> 
+                 by trading land, fiding treasures and building businessess. Welcome!
+            </p>
+            <Button text="Join our Discord" icon={<RiGroupLine />} />
+        </div>
+        <div className="image">
+            <img src={image} alt="Home " />
+        </div>
+       </Section>
+    );
+
+}
+
+const Section = styled.section`
+background-image: url(${background});
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  display: grid;
+  grid-template-columns: 50% 50%;
+  height: 80vh;
+  align-items: center;
+  .text {
+    padding-left: 10rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 1rem;
+    .highlight {
+      color: var(--primary-color);
+    }
+    h1 {
+      font-size: 4.8vw;
+    }
+    p {
+      font-size: 1.5vw;
+    }
+  }
+  .image {
+    img {
+      width: 80%;
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    display: flex;
+    flex-direction: column-reverse;
+    height: max-content;
+    text-align: center;
+    margin: 0 1rem;
+    .image {
+      img {
+        width: 100%;
+      }
+    }
+    .text {
+      padding-left: 0;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      h1 {
+        font-size: 8vw;
+      }
+      p {
+        font-size: 5vw;
+      }
+    }
+  }
+`;
